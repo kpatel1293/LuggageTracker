f7f9231c50e6f1d150af53305442c1a1f7a9263d
kedepena <kedepena@gmail.com>
2021-05-05 21:54:22 -0600

    Minor updates to settings and database.

--- LuggageTrackerWebApp/urls.py
+++ LuggageTrackerWebApp/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [ 
-    path('', views.home, name='home'), #home page , the home() function will be define in "views.py", which is called to return something to client
-    path('add', views.add, name='add') #add fucntion called when user submits, the fucntion is defined in "views.py"
+    path('', views.home, name='home'), #home page, the home() function will be define in "views.py", which is called to return something to client
+    path('add/', views.add, name='add') #add function called when user submits, the function is defined in "views.py"
 ]
