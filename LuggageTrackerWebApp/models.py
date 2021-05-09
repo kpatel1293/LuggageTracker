ef474ede3384561fca84023a778b8f86a6eb208d
Jack Liu <55923376+Chien09@users.noreply.github.com>
2021-05-09 18:03:14 +0700

    Web Interface edits
    home.html
    views.py
    models.py
    result.html

--- LuggageTrackerWebApp/models.py
+++ LuggageTrackerWebApp/models.py
@@ -8,30 +8,11 @@ class luggage:
     Description : str
     TimeStamp : str
     Origin_Airport : str
-    Transit_Airport : str
+    InTransit_Status : str
+    Current_Location : str 
     Destination_Airpot : str
       
 
-#Example
-#this code will be included in "views.py"
-
-#form .models import luggage 
-
-#def index(request):
-#    object1 = luggage() 
-#    object1.Origin_Airport = 'O'HARE International Airport'
-
-#    dests = [object1, object2]  #this will allow you to pass multiple datas in array form, just change bleow {'dests' : dests}
-
-#    return render(request, "home.html", {'object1' : object1})
-
-
-#in the html file you will need to include a for loop to retrieve the dynamic array of objects to display on webpage
-# {% for dest in dests %}
-#
-# you html code 
-#
-# {% endfor %}
 
 
     
--- LuggageTrackerWebApp/views.py
+++ LuggageTrackerWebApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import luggage #importing luggage data class 
+import datetime
 
 # Create your views here. This is include your business logic code to send the data to create the HTML website page 
 
@@ -8,12 +10,40 @@ def home(request):
     return render(request, 'home.html') #rendering the webpage home to client
 
 
-#search() called when user submits LuggageTagID
+#search() called when user submits LuggageTagID on home.html page 
 def search(request):
 
     Luggage_TagID : str = request.POST["LuggageTagID"] #using POST instead of GET which is more secure 
 
-    return render(request, 'result.html', {'result': Luggage_TagID}) #rendering the webpage, sending the result
+    #needs to retreive luggage information from database, so for now this is dummay data below for demonstration purposes 
+    Luggage1 = luggage()
+    Luggage1.TagID ='ABC12345'
+    Luggage1.Description = 'Samsonite EVOA ICE Blue'
+    Luggage1.TimeStamp = datetime.date.today()
+    Luggage1.Origin_Airport = 'Portland International Airport (PDX)'
+    Luggage1.InTransit_Status = 'Arrived Destination'
+    Luggage1.Destination = 'Chicago O Hare International Airport (ORD)'
+    Luggage1.Current_Location = 'Chicago O Hare International Airport (ORD)'
+
+    Luggage2 = luggage()
+    Luggage2.TagID = '12345ABC'
+    Luggage1.Description = 'American Tourister LINEX Green'
+    Luggage1.TimeStamp = datetime.date.today()
+    Luggage1.Origin_Airport = 'Los Angeles International Airport (LAX)'
+    Luggage1.InTransit_Status = 'In Transit'
+    Luggage1.Destination = 'John F. Kennedy International Airport (JFK)'
+    Luggage1.Current_Location = 'Seattle-Tacoma International Airport (SEA)'
+    
+    #Add the luggage objects to array this is dummy data, normal we will be retrieving from database 
+    luggages = [Luggage1, Luggage2]
+
+    #forloop to find the matching user input Luggage TagID 
+    foundLuggage : luggage
+    for L in luggages:
+        if(Luggage_TagID == L.TagID):
+            foundLuggage = L
+
+    return render(request, 'result.html', {'LuggageObject': foundLuggage}) #rendering the webpage, sending the result
 
 
 #add button clicked on homepage takes you to the "add.html" webpage 
