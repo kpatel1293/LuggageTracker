92adb3c7821ab68829ae92a97bede9a32f19b2e7
Krishna Patel <kpatel1293@gmail.com>
2021-05-21 20:10:02 -0400

    fix

--- LuggageTrackerWebApp/urls.py
+++ LuggageTrackerWebApp/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
     path('search', views.search, name='search'), #search() function called when user submits Luggage Tag ID, function defined in "views.py"
     path('movetoadd', views.movetoadd, name='movetoadd'), #movetoadd() function called when user clicks on add button on homepage, function defined in "views.py"
     path('addLuggage', views.addLuggage, name='addLuggage'), #addLuggage() function called when user submits a new luggage, function defined in "views.py"
-    ######################
     path('movetologin', views.movetologin, name='movetologin'), #movetologin() function called when user clicks on login button to take to login in page
     path('login', views.login, name='login'), #login() function called with user signin take back to home page 
-    ######################
-    path('testLuggage', views.testLuggage, name='testLuggage'),
-    path('create', views.create, name='create')
 ]
--- LuggageTrackerWebApp/views.py
+++ LuggageTrackerWebApp/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import * #importing luggage data class 
+from django.shortcuts import render
+from .models import luggage #importing luggage data class 
 import datetime
-######################
-import string
-import random
-######################
-from .blockchain_org import *
 
 # Create your views here. This is include your business logic code to send the data to create the HTML website page 
 
@@ -76,48 +71,13 @@ def movetoadd(request):
 #user submit, adding new Luggage information 
 def addLuggage(request):
 
-    #retrieve user input 
-    Luggage_Description : str = request.POST["Description"]
-    Origin_Airport : str = request.POST["originAirport"]
-    Destination_Airport : str = request.POST["desAirport"]
-    Transit_Airport : str = request.POST["transitAirport"]
+    #need to create Luggage object and append all the data submitted from client, as well as, adding to database and blockchain
+    #maybe auto generate Tag ID????????????????? 
+    #need to store timestamp 
+    #return an array of data, need to send it also 
 
-    #Auto generate tag ID
-    Tag_ID : str = tagID_generator(); 
+    return render(request, 'addresult.html')
 
-    #Date time 
-    Current_DateTime : str = datetime.datetime.now()
-
-    #Status ('Arrived' -> Arrived, 'In Transit' -> In Transit)
-    Luggage_Status : str = 'In Transit'
-
-    #Flagged ('N' -> 'N', 'Y' -> 'Y')
-    Luggage_Flagged : str = 'N'
-
-    #Digital Signature ('Approved' -> Approved, 'Disapproved' -> Disapproved, 'Waiting' -> Waiting)
-    Digital_sig = 'Waiting'
-
-    #Create the new luggage object 
-    Luggage_obj = luggage(
-        tag_id = Tag_ID, 
-        description = Luggage_Description, 
-        time_stamp = Current_DateTime, 
-        origin_airport = Origin_Airport,
-        transit_airport = Transit_Airport,
-        destination_airport = Destination_Airport,
-        status = Luggage_Status,
-        flagged = Luggage_Flagged,
-        digital_signature = Digital_sig
-    )
-
-    #Push the Luggage Object onto the Mysql database 
-    Luggage_obj.save()
-
-    return render(request, 'addresult.html', {'Luggage_Object' : Luggage_obj})
-
-#auto generate Tag ID based on numbers and captial alphabets 
-def tagID_generator(size=20, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 #user clicks on login button takes to login.html page 
 def movetologin(request):
@@ -128,50 +88,4 @@ def login(request):
     
     #code to process sign in 
 
-######################
     return render(request, 'home.html')
-######################
-    #return render(request, 'addresult.html')
-
-def testLuggage(request):
-
-    luggageBlock = LuggageBlockchain.objects.all()  #gets all luggage objects in database 
-
-    context = {
-        'luggage' : luggageBlock
-    }
-
-    return render(request, 'testpage.html', context)
-
-def create(request):
-    # validate users
-    valid, result = LuggageBlockchain.objects.validateRegister(request.POST)
-
-    # print(result)
-
-    # luggage = LuggageBlockchain.objects.all()
-
-    # pending = {'transactions':[]}
-    # transit = {'transactions':[]}
-    # arrived = {'transactions':[]}
-    
-    # testBlock = Blockchain()
-    
-    # for l in luggage:
-    #     # di = {"tagID":l.tag_id, "name":l.name, "transit":l.transit}  
-    #     if l.transit == 'T':
-    #         transit['transactions'].append({"tagID":l.tag_id, "name":l.name, "transit":l.transit})
-    #         a = testBlock.add(transit)
-    #     elif l.transit == 'A':
-    #         arrived['transactions'].append({"tagID":l.tag_id, "name":l.name, "transit":l.transit})
-    #         b = testBlock.add(arrived)
-    #     else:
-    #         pending['transactions'].append({"tagID":l.tag_id, "name":l.name, "transit":l.transit})
-    #         c = testBlock.add(pending)
-    
-    # t = testBlock.getTransactions('all')
-
-    # print(t)
-
-    # redirect to user dashboard
-    return redirect('testLuggage')
