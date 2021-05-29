ca4a17fcf2c8c2ac0a247a699a3dd71ed871222c
Krishna Patel <kpatel1293@gmail.com>
2021-05-28 21:50:43 -0400

    added blockchain functionality

--- LuggageTrackerWebApp/models.py
+++ LuggageTrackerWebApp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import TimeField
-from datetime import datetime
+# from datetime import datetime
+# import time
+from django.utils import timezone
+
 
 # Create your models here. Create data classes here and objects will be created in "views.py" and passing the object to htmls
 
@@ -12,7 +16,6 @@ class LuggageManager(models.Manager):
         # store user input new luggge to database 
         addLuggage = self.create(
             description=form_data['description'],
-            time_stamp=datetime.now(),
             origin_airport=form_data['origin_airport'],
             transit_airport=form_data['transit_airport'],
             destination_airport=form_data['destination_airport']
@@ -21,10 +24,10 @@ class LuggageManager(models.Manager):
         return addLuggage.tag_id
 
 #for migrating data to MYSQL database      
-class luggage(models.Model):
+class Luggage(models.Model):
     tag_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     description = models.TextField(blank=True)
-    time_stamp = models.DateTimeField('Time Last Scanned', default=datetime.now())
+    time_stamp = models.TimeField('Time Last Scanned', default=timezone.now)
     origin_airport = models.CharField(max_length=150)
     transit_airport = models.CharField(max_length=150)
     destination_airport = models.CharField(max_length=150)
@@ -46,7 +49,41 @@ class luggage(models.Model):
     def __str__(self):
         return str(self.tag_id)
 
-class airport(models.Model):
+class BlockchainManager(models.Manager):
+    # validate registeration
+    def validateBlockchain(self,blockchain, luggage):
+        # store user input new luggge to database 
+        addBlock = self.create(
+            index=blockchain['index'],
+            transactions=luggage,
+            timestamp=blockchain['timestamp'],
+            prevHash=blockchain['prevHash'],
+            nonce=blockchain['nonce'],
+            hash_curr=blockchain['hash']
+        )
+
+        return addBlock.hash_curr
+
+    def updateBlockchain(self,id,blockchain):
+        updateBlock = self.filter(hash_curr=id).update(index=blockchain['index'],timestamp=blockchain['timestamp'],prevHash=blockchain['prevHash'],nonce=blockchain['nonce'],hash_curr=blockchain['hash'])
+        return blockchain['hash']
+
+class Blocks(models.Model):
+    index = models.IntegerField(primary_key=True)
+    transactions = models.OneToOneField(Luggage, on_delete=CASCADE)
+    timestamp = models.FloatField()
+    prevHash = models.CharField(max_length=150)
+    nonce = models.IntegerField(default=0)
+    hash_curr = models.CharField(max_length=150)
+
+    # call luggage manager
+    objects = BlockchainManager()
+
+    # print user created
+    def __str__(self):
+        return str(self.hash_curr)
+
+class Airport(models.Model):
     name = models.TextField(blank=True)
     municipality = models.CharField(max_length=150)
     iatacode = models.CharField('IATA Code', max_length=10)
--- LuggageTrackerWebApp/views.py
+++ LuggageTrackerWebApp/views.py
@@ -1,6 +1,61 @@
 from django.shortcuts import redirect, render
 from .models import *  # importing luggage data class
 from django.http import JsonResponse
+from .blockchain.blockchain import *
+
+bc = Blockchain()
+
+# get chain
+def getChain():
+    blockchain = []
+    for block in bc.chain:
+        blockchain.append(block.__dict__)
+    return {"length": len(blockchain), "LuggageBlockchain" : blockchain}
+
+def getLast():
+    return getChain()['LuggageBlockchain'][getChain()['length']-1]
+
+def checkBlockchain():
+    chainLength = bc.getSize()
+    try:
+        luggageLength = Luggage.objects.count()
+    except:
+        return False
+
+    if chainLength == 1 and luggageLength == 0:
+        return False
+
+    if chainLength == 1 and luggageLength > 0:
+        luggageList = Luggage.objects.all()
+        for l in luggageList:
+            block = { 
+                    'tag_id' : l.tag_id,
+                    'description' : l.description, 
+                    'origin_airport' : l.origin_airport, 
+                    'transit_airport' : l.transit_airport, 
+                    'destination_airport' : l.destination_airport,
+                    'status' : l.status,
+                    'flag' : l.flagged,
+                    'digital_signature' : l.digital_signature
+                }
+
+            bc.blockchainTransactions(block)
+            bc.mine()
+            
+            if Blocks.objects.filter(transactions_id = l.tag_id).exists():
+                blockUpdate = Blocks.objects.updateBlockchain(Blocks.objects.get(transactions_id = l.tag_id), getLast())
+            else:
+                addBlock = Blocks.objects.validateBlockchain(getLast(),l)
+
+        # print(getChain())
+        return True
+
+    return False
+
+try:
+    checkBlockchain()
+except:
+    print('database is not created, comment all functions that are above this line')
 
 # Create your views here. This is include your business logic code to send the data to create the HTML website page
 # the request object is from the client and we will be returning something to client webpage
@@ -11,10 +66,9 @@ def home(request):
 
 #search() called when user submits LuggageTagID on home.html page 
 def search(request):
-
     # Luggage_TagID : str = request.POST["LuggageTagID"] #using POST instead of GET which is more secure 
     try:
-        result = luggage.objects.get(tag_id=request.POST["tag_id"])
+        result = Luggage.objects.get(tag_id=request.POST["tag_id"])
         return redirect('result/{}'.format(result))
     except:
         return redirect('notfound') #when Luggage Tag ID not found 
@@ -22,10 +76,9 @@ def search(request):
 
 #posting the user search resutls of luggage found from database
 def result(request, tag_id):
-    lug = luggage.objects.all()
-    for l in lug:
-        if l.tag_id == tag_id:
-            context = {'LuggageObject': l}
+    if not Luggage.objects.filter(tag_id=tag_id).exists():
+        return redirect('notfound')
+    context = {'LuggageObject': Luggage.objects.get(tag_id=tag_id), 'Blockchain': Blocks.objects.get(transactions_id=tag_id)}
 
     return render(request, 'result.html', context)
 
@@ -37,20 +90,36 @@ def notfound(request):
 #created new luggage from user input and push to database 
 def createLuggage(request):
     try:
-        result = luggage.objects.validateLuggage(request.POST) #push data to database based on user input
-        return redirect('addLuggage/{}'.format(result))
+        result = Luggage.objects.validateLuggage(request.POST) #push data to database based on user input
+        l = Luggage.objects.get(tag_id=result)
+
+        block = { 
+            'tag_id' : l.tag_id,
+            'description' : l.description, 
+            'origin_airport' : l.origin_airport, 
+            'transit_airport' : l.transit_airport, 
+            'destination_airport' : l.destination_airport,
+            'status' : l.status,
+            'flag' : l.flagged,
+            'digital_signature' : l.digital_signature
+        }
+            
+        bc.blockchainTransactions(block)
+        bc.mine()
+
+        b = Blocks.objects.validateBlockchain(getLast(),l)
+
+        return redirect('result/{}'.format(result))
     except:
         return redirect('addfailed')
 
+# print(getLast())
 
 #direct to confirmation page of new added luggage
-def addLuggage(request, tag_id):
-    lug = luggage.objects.all()
-    for l in lug:
-        if l.tag_id == tag_id:
-            context = {'LuggageObject': l}
-
-    return render(request, 'addresult.html', context)
+# def addLuggage(request, tag_id):
+#     context = {'LuggageObject': Luggage.objects.get(tag_id=tag_id)}
+    
+#     return render(request, 'addresult.html', context)
 
 
 #for redirects when new Luggage add or push fails 
@@ -72,7 +141,7 @@ def faq(request):
 
 def autocomplete(request):
     if 'term' in request.GET:
-        qs = airport.objects.filter(name__icontains=request.GET.get('term'))
+        qs = Airport.objects.filter(name__icontains=request.GET.get('term'))
         names = list()
         for ap in qs:
             names.append(ap.name)
